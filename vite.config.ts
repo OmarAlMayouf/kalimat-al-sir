da0508a9e7d5a9a206c198914858f5b2595afc89
Omar Al Mayouf <omaralmayouf@gmail.com>
2026-03-01 01:03:23 +0300

    change repo name

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/saudia-connect-play/',
+  base: '/kalimat-al-sir/',
   server: {
     host: "::",
     port: 8080,
